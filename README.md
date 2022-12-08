828daa424edb1505bc736522f27f1dfac1502a47
Moishi Netzer <moishinetzer@gmail.com>
2022-12-08 15:59:13 +0000

    Add Button

---
+++ README.md
@@ -0,0 +1,33 @@
+# Compote
+
+![Compote Logo](/public/logo-small.png)
+
+The Stylistic Reusable Component Framework
+
+## Installation
+
+```sh
+npx compote init
+```
+
+## Description
+
+- Explain what Compote solves
+
+- Explain what it runs on (vite)
+
+## What's included
+
+- Storybook
+- (Other defaults)
+
+## Integrations
+
+- TailwindCSS
+- ChakraUI
+
+## Best Practices
+
+- Always use forwardRef's
+
+## Notes
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Button } from "./components/Button";
+import { Button } from "./stories/Button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {/* The Reusable Button */}
+        <Button
+          onClick={() => setCount((count) => count + 1)}
+          label="Click ME"
+          backgroundColor="#9be0ae"
+          primary={false}
+          size="large"
+        />
+
+        <br />
+        <br />
+        <p>Count: {count}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Button />
     </div>
   );
 }
