4bc80dba251d0b0cd27bd5e7d13a277818a1a1b5
Moishi Netzer <moishinetzer@gmail.com>
2023-01-24 12:45:16 +0000

    Change to css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
 import { Button } from "./Button";
 
 function App() {
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,3 +1,5 @@
+import styles from "./index.module.css";
+
 export function Button() {
-  return <button>This is the pbandj button</button>;
+  return <button className={styles.button}>This is the pbandj button</button>;
 }
