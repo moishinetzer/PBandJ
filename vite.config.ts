cc1e9eacc3cc88674eb204347012b9f290956b5c
Moishi Netzer <moishinetzer@gmail.com>
2023-01-24 15:35:09 +0000

    Make those changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts(), cssInjectedByJsPlugin()],
   // Build components from src/index.ts
   build: {
     lib: {
