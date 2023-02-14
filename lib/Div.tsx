84d4b1ef36a4bc26705ce9014f7551266e7b9f37
Moishi Netzer <moishinetzer@gmail.com>
2023-02-14 18:05:04 +0000

    update storybook

--- lib/Div.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./Div.module.css";
-
-export function Div() {
-  return <div className={styles.div}>Div Word</div>;
-}
--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -1,7 +1,7 @@
 import { Meta } from "@storybook/react";
 import type { StoryObj } from "@storybook/react";
 
-import { Button } from "./Button";
+import { Button } from "../lib";
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 const meta = {
